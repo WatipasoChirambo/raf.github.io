e02e665a1809d9d4b9c66a089239bec140501486
RattleStack <76850781+WatipasoChirambo@users.noreply.github.com>
2021-06-16 07:08:09 +0200

    Services page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,9 +5,12 @@ export default function About() {
   return (
       <Layout>
     <section>
-      <p>
-       About
-      </p>
+      <div>
+        <h2>About US</h2>
+      </div>
+      <div>
+        <p>40Pounds</p>
+      </div>
     </section>
     </Layout>
   )
