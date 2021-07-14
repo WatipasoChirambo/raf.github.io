a2186b6cb78edf5f1a79db0b959b16cc8524892c
RattleStack <76850781+WatipasoChirambo@users.noreply.github.com>
2021-07-14 16:29:52 +0200

    Icons

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Logo from '../Navbar/Logo';
 import { Link } from "gatsby";
 import { Container,Navbar, Nav } from 'react-bootstrap';
+import * as styles from '../../styles/navigation.module.css'
 
 function Navigationbar() {
     return (
@@ -13,22 +14,22 @@ function Navigationbar() {
                 <Navbar.Toggle aria-controls="navbarResponsive"/>
                 <Navbar.Collapse id="navbarResponsive">
                     <Nav as="ul" className="ml-auto">
-                        <Nav.Item as="li">
+                        <Nav.Item as="li" className={styles.navStyle}>
                             <Link to="/" className="nav-link" activeClassName="active">
                                 Home
                             </Link>
                         </Nav.Item>
-                        <Nav.Item as="li">
+                        <Nav.Item as="li" className={styles.navStyle}>
                             <Link to="/Products" className="nav-link" activeClassName="active">
                                 Products
                             </Link>
                         </Nav.Item>
-                        <Nav.Item as="li">
+                        <Nav.Item as="li" className={styles.navStyle}>
                             <Link to="/services" className="nav-link" activeClassName="active">
                                 Services
                             </Link>
                         </Nav.Item>
-                        <Nav.Item as="li">
+                        <Nav.Item as="li" className={styles.navStyle}>
                             <Link to="/about" className="nav-link" activeClassName="active">
                                 About
                             </Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import { Link } from 'gatsby';
 import React from "react"
-import { Carousel } from 'react-bootstrap';
+import Img from "gatsby-image";
+import { Carousel, Col } from 'react-bootstrap';
 import Layout from '../components/Layout'
 import * as styles from '../styles/home.module.css'
 import * as style_ting from '../styles/about.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {   faSeedling } from '@fortawesome/free-brands-svg-icons';
-import { StaticImage } from "gatsby-plugin-image"
-import { Card,ListGroup,ListGroupItem } from 'react-bootstrap';
+import {  faFacebookF , faTwitter,faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { StaticImage } from "gatsby-plugin-image";
+import { Card,ListGroup,ListGroupItem,Container,Row } from 'react-bootstrap';
 // import vision from "../images/vision.png"
 import mission from "../images/mission.png"
 import objectives from "../images/goal.png"
@@ -24,7 +25,7 @@ export default function Home() {
           <div className={styles.slogan}>
             <div>
               <div className={styles.hero}>
-                <h1>Fresh Farm Produce <br></br> <span style={{ color: `green` }}>For You</span></h1>
+                <h2>Fresh Farm Produce <br></br> <span style={{ color: `green` }}>For You</span></h2>
               </div>
               <div className={styles.hero_text}>
                 <p>Ritz farms and Property investment Limited sets out itself to be a leading agribusiness in Malawi.
@@ -42,111 +43,116 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div style={{ textAlign: `center`, maxWidth:`auto`, maxHeight:`150`, marginLeft:`32%`}}>
+          <div style={{ textAlign: `center`, maxWidth:`auto`, maxHeight:`150`}}>
             <StaticImage src="../images/environment.png" alt="farmer"/>
           </div>
         </div>
         <div className={styles.company}>
           <h3>Company Information</h3>
           <div className={styles.company_core_businesses}>
-            <div className={styles.vision}>
-              <h5>Vision</h5>
-              <Card style={{ width: '18rem', border:`none` }}>
-                {/* <Card.Img variant="top" src={Vision} /> */}
-                <Card.Body>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
+          <div className={styles.right_middle}>
+            <h5>Vision</h5>
+            <div style={{ width: '200' }}>
+              <StaticImage src="../images/eye.png" alt="farmer"/>
             </div>
-            
-            <div className={styles.mission}>
-              <h5>Mission</h5>
-              <Card style={{ width: '18rem', border:`none` }}>
-              <Card.Img variant="top" src={mission} style={{ textAlign: `center`, maxWidth:`auto`, maxHeight:`100`}} /> 
-              <Card.Body>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
-                </Card.Text>
-              </Card.Body>
-          </Card>
-            </div>
+            <Card style={{ width: '100%' }}>
+                  <Card.Body>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the bulk of
+                      the card's content.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+          </div>
 
-            <div className={styles.objectives}>
-              <div className={styles.middle_middle}>
-              <h5>Objectives</h5>
-                <Card style={{ width: '18rem', border:`none` }}>
-                <Card.Img variant="top" src={objectives} />
-                <Card.Body>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              </div>
+          <div className={styles.left_middle}>
+            <h5>Mission</h5>
+            <div style={{ width: '200' }}>
+              <StaticImage src="../images/objectives.png" alt="farmer"/>
             </div>
+            <Card style={{ width: '100%' }}>
+                  <Card.Body>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the bulk of
+                      the card's content.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+          </div>
+
+          <div className={styles.left_middle}>
+            <h5>Objectives</h5>
+            <div style={{ width: '200' }}>
+              <StaticImage src="../images/tasks.png" alt="farmer"/>
+            </div>
+            <Card style={{ width: '100%' }}>
+                  <Card.Body>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the bulk of
+                      the card's content.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+          </div>
           </div>
         </div>
+        <hr></hr>
         <div className={styles.middle_heading}>
           <h3>Core Businesses</h3>
         </div>
         <div className={styles.middle}>
-          <div className={styles.left_middle}>
-          <h5>Real Estate</h5>
-          <Card style={{ width: '18rem', border:`none` }}>
-            <Card.Img variant="top" src={agr_trade} />
-            <Card.Body>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroupItem>Cras justo odio</ListGroupItem>
-              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-              <ListGroupItem>Vestibulum at eros</ListGroupItem>
-            </ListGroup>
-          </Card>
+          <Link style={{ textDecoration: `none` }} to='/'>
+          <div className={styles.right_middle}>
+              <h5>Agricultural Trade</h5>
+              <div style={{ width: '200', color:`green` }}>
+                <StaticImage src="../images/seedling.png" alt="farmer"/>
+              </div>
+              <Card style={{ width: '100%' }}>
+                    <Card.Body>
+                      <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+            </div>
+          </Link>
+
+          <Link style={{ textDecoration: `none` }} to='/'>
+          <div className={styles.right_middle}>
+            <h5>Livestock</h5>
+            <div style={{ width: '200' }}>
+              <StaticImage src="../images/eye.png" alt="farmer"/>
+            </div>
+            <Card style={{ width: '100%' }}>
+                  <Card.Body>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the bulk of
+                      the card's content.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
           </div>
-          <div className={styles.middle_middle}>
-          <h5>Agriculture Trade</h5>
-          <Card style={{ width: '18rem', border:`none` }}>
-            <Card.Img variant="top" src={agr_trade} />
-            <Card.Body>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroupItem>Cras justo odio</ListGroupItem>
-              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-              <ListGroupItem>Vestibulum at eros</ListGroupItem>
-            </ListGroup>
-          </Card>
-          </div>
+          </Link>
+
+          <Link style={{ textDecoration: `none` }} to='/'>
           <div className={styles.right_middle}>
             <h5>Logistics</h5>
-            <Card style={{ width: '18rem', border:`none`}}>
-            <Card.Img variant="top" src={logistics} />
-            <Card.Body>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-              <ListGroupItem>Cras justo odio</ListGroupItem>
-              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-              <ListGroupItem>Vestibulum at eros</ListGroupItem>
-            </ListGroup>
-          </Card>
+            <div style={{ width: '200' }}>
+              <StaticImage src="../images/shipping.png" alt="farmer"/>
+            </div>
+            <Card style={{ width: '100%' }}>
+                  <Card.Body>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the bulk of
+                      the card's content.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
           </div>
+          </Link>
         </div>
+        <hr></hr>
     </Layout>
   )
 }
