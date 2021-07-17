e56b412a05a8feef08b7ec06ca8b1d70d10df15a
RattleStack <76850781+WatipasoChirambo@users.noreply.github.com>
2021-07-17 21:15:01 +0200

    Button-outline-secondary

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 import Layout from '../components/Layout'
 import * as styles from '../styles/home.module.css'
 import { StaticImage } from "gatsby-plugin-image";
-import { Card } from 'react-bootstrap';
+import { Card,Button } from 'react-bootstrap';
 
 
 export default function Home() {
